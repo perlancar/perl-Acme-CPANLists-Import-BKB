3a9f087b7b8e7554e65577a53090c2a93f86a006
perlancar (on pc-office) <perlancar@gmail.com>
2017-03-27 15:56:52 +0700

    Initial commit

---
+++ lib/Acme/CPANLists/Import/BKB.pm
@@ -0,0 +1,18 @@
+package Acme::CPANLists::Import::BKB;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CPANLists imported from Ben Bullock's website
+
+=head1 DESCRIPTION
+
+This distribution contains module lists that are imported from Ben Bullock's
+(CPANID:BKB) website posts located at L<https://www.lemoda.net/>. This
+distribution is created to improve the "mentions cloud".
+
+
+=head1 append:SEE ALSO
+
+L<App::lcpan> and L<lcpan>, particularly the B<related-mods> subcommand.
